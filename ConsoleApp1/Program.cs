321c1bc27c68284f6037e5544e510600b61fb2b5
kryuchkovdm <dimasic8950456@mail.ru>
2021-10-13 07:41:02 +0300

    commit
    Add new writeline

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello Colleaques! =)");
             Console.WriteLine("Test GitHub");
+            Console.WriteLine("Hello bros");
         }
     }
 }
